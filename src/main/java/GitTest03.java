9e0d093b5a375d4fd6bcb142930daf20b1499dc6
John <13178389303@163.com>
2022-02-28 12:55:10 +0800

    git_test_03 first commit

--- src/main/java/GitTest03.java
+++ src/main/java/GitTest03.java
@@ -1,5 +1,6 @@
 public class GitTest03 {
     public static void main(String[] args) {
         System.out.println("git_test_03 first share");
+        System.out.println("git_test_03 first push");
     }
 }
