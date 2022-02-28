58c508795b347ee7cd18320aa0f1ca37d8731f74
RecursionMan <94585073+RecursionMan@users.noreply.github.com>
2022-02-28 12:56:59 +0800

    git_test_03 pull test

--- src/main/java/GitTest03.java
+++ src/main/java/GitTest03.java
@@ -2,5 +2,6 @@ public class GitTest03 {
     public static void main(String[] args) {
         System.out.println("git_test_03 first share");
         System.out.println("git_test_03 first push");
+        System.out.println("git_test_03 first pull");
     }
 }
